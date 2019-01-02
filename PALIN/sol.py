df5e33b612281616385629d43cd5c7674cf9590d
maitreyasinha <maitreya@live.in>
2019-01-02 20:27:16 +0530

    Removed list conversion to save memory

--- PALIN/sol.py
+++ PALIN/sol.py
@@ -1,46 +1,42 @@
 n, output = int(input()), ""
 
-def compare(num1,num2):
-    if len(num1)==0 or len(num2)==0:
-        return True
-    elif int(num1[-1]) > int(num2[0]):
-        return False
-    elif int(num1[-1]) < int(num2[0]):
-        return True
-    else:
-        return compare(num1[:-1:],num2[1::])
 
-def incrment(num1):
-    if len(num1)==0:
+def compare(num1, num2):
+    for i in range(len(num2)):
+        if num1[-1-i] > num2[i]:
+            return False
+        elif num1[-1-i] < num2[i]:
+            return True
+        else:
+            continue
+    return True
+
+
+def increment(num1):
+    if len(num1) == 0:
         return ["1"]
-    elif num1[-1]=="9":
-        return incrment(num1[:-1])+["0"]
+    elif num1[-1] == "9":
+        return increment(num1[:-1])+["0"]
     else:
         return num1[:-1] + [str(int(num1[-1])+1)]
 
+
 def convert(num):
     if num == ["9"]:
-        return ["1","1"]
-    if len(num)%2==0:
-        center = int(len(num)/2)
-        left = num[:center]
-        if compare(left,num[center::]):
-            left = incrment(left)
+        return ["1", "1"]
+    center = int(len(num)/2)
+    left = num[:center]
+    if len(num)%2 == 0:
+        left = increment(left) if compare(left, num[center::]) else left
         return left + left[-1-len(left)+len(num[center:])::-1]
     else:
-        center = int(len(num)/2)
-        left = num[:center]
-        if compare(left,num[center+1::]):
-            left = incrment(num[:center+1])
-        else:
-            left = num[:center+1]
-        num = left + left[-2-len(left) + center + 1 ::-1]
-        return num
+        left = increment(num[:center+1]) if compare(left, num[center+1::]) else num[:center+1]
+        return left + left[-2-len(left) + center + 1::-1]
 
 
 for i in range(n):
-    _inp = list(input())
-    output = output + "\n" + "".join(convert(list(_inp)))
+    _inp = input()
+    output = output + "\n" + "".join(convert(_inp))
 
 
 print(output)
